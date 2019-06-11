57cdb6fa127f27bcbaca95488db167a2737e0231
nanmiao <nanmiaowu@gmail.com>
2019-06-11 14:48:23 -0500

    first commit

---
+++ Sec13/exer_13_0_1/cmake-build-debug/CMakeFiles/exer_13_0_1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exer_13_0_1.dir/main.cpp.o"
+  "CMakeFiles/exer_13_0_1.dir/Core.cpp.o"
+  "CMakeFiles/exer_13_0_1.dir/grade.cpp.o"
+  "CMakeFiles/exer_13_0_1.dir/median.cpp.o"
+  "CMakeFiles/exer_13_0_1.dir/Student_info.cpp.o"
+  "exer_13_0_1.pdb"
+  "exer_13_0_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exer_13_0_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sec13/exer_13_0_1/grade.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Nanmiao Wu on 2019-05-06.
+//
+#include <vector>
+#include "grade.h"
+#include "median.h"
+#include <stdexcept>
+
+using namespace std;
+
+double grade(double midterm, double final, double homework){
+    return 0.2 * midterm + 0.4 * final + 0.4 * homework;
+}
+
+double grade(double midterm, double final, const vector<double>& hw){
+    if(hw.size() == 0)
+        throw domain_error("student has done no homework");
+    return grade(midterm, final, median(hw));
+}
