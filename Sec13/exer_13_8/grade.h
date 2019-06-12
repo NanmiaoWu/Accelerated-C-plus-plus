18edd3417344f6f9658ab0250d509442c41eaafc
nanmiao <nanmiaowu@gmail.com>
2019-06-12 14:00:34 -0500

    add exer_13_8

---
+++ Sec13/exer_13_8/grade.h
@@ -0,0 +1,13 @@
+//
+// Created by Nanmiao Wu on 2019-05-06.
+//
+
+#ifndef EXER_9_0_GRADE_H
+#define EXER_9_0_GRADE_H
+
+#include <vector>
+
+double grade(double, double, double);
+double grade(double, double, const std::vector<double>&);
+
+#endif //EXER_9_0_GRADE_H
