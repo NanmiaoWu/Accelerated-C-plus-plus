4a23cff083c16b71962de7768a5c3a9f2a88f996
nanmiao <nanmiaowu@gmail.com>
2019-06-26 09:54:38 -0500

    add exer_16_0

---
+++ Sec16/exer_16_0/Student_info.h
@@ -0,0 +1,44 @@
+//
+// Created by nanmiao on 6/11/19.
+//
+
+#ifndef EXER_13_0_1_STUDENT_INFO_H
+#define EXER_13_0_1_STUDENT_INFO_H
+
+#include "Core.h"
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class Student_info {
+public:
+    // constructors and copy control
+    Student_info(): cp(0) { }
+    Student_info(std::istream& is): cp(0) { read(is); }
+    Student_info(const Student_info&);
+    Student_info& operator=(const Student_info&);
+    ~Student_info() { delete cp; }
+
+    // operations
+    std::istream& read(std::istream&);
+
+    std::string name() const {
+        if (cp) return cp->name();
+        else throw std::runtime_error("uninitialized Student");
+    }
+    double grade() const {
+        if (cp) return cp->grade();
+        else throw std::runtime_error("uninitialized Student");
+    }
+
+    static bool compare(const Student_info& s1,
+                        const Student_info& s2) {
+        return s1.name() < s2.name();
+    }
+
+private:
+    Core* cp;
+};
+
+#endif //EXER_13_0_1_STUDENT_INFO_H
